6f377338d9a8562f9732bdc10e57ebb4a40d4ebf
shubhamr10 <shubhamrauniyar10@gmail.com>
2021-07-05 08:35:18 +0530

    Add Hello Java & Initial Setup

---
+++ CoreJava/HelloJava/HelloJava.java
@@ -0,0 +1,12 @@
+//import java.lang.System;
+//import java.lang.String;
+
+//We Can either import all the java pre-built classes like this. or we
+import java.lang.*;
+
+class HelloWorld{
+    public static void main(String args[]){
+        System.out.println("******************Hello Java**********************");
+    }
+}
+
---
+++ CoreJava/Programs/Program1.java
@@ -0,0 +1,1 @@
+/* Write a Java Program to find
