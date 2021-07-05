08ec44d50bf8207ab8cfccf10022046579509f6b
shubhamr10 <shubhamrauniyar10@gmail.com>
2021-07-05 08:50:30 +0530

    Completed Sum Program

--- CoreJava/Programs/Program1.java
+++ CoreJava/Programs/Program1.java
@@ -17,5 +17,12 @@ class Program1Sum{
         System.out.println();
         System.out.println("Sum of numbers is:"+z);
 //        Trying to calculate and print on the same line
+        System.out.println("Sum of number is calculated as"+x+y);
+//        You will see the output as 1025 . It is because of the type casting precedence.
+//        which is done from the left hand side. It means:
+//        "Sum of number is calculated as" - It is the left most value of type string.
+//        So, type string is set as precedene. In order to solve it properly we
+//        will have to use the ()
+        System.out.println("Sum of number is calculated as"+(x+y));
     }
 }
