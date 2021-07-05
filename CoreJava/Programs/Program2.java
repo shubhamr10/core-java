b956be6ca9f74772f238b00bead36ede5b88730b
shubhamr10 <shubhamrauniyar10@gmail.com>
2021-07-05 09:00:52 +0530

    Program 2: Backslash code

---
+++ CoreJava/Programs/Program2.java
@@ -0,0 +1,25 @@
+/* Write a program to demonstrate all the backlash code */
+/*
+* |  Backslash Code  |  Meaning     |
+* 1. \n ==> New Line
+* 2. \t ==> Horizontal tab space
+* 3. \r ==> Enter Key
+* 4. \b ==> Backspace
+* 5. \f ==> Form feed
+* 6. \\ ==> Displays \ (Single \);
+* 7. \" ==> Displays "
+* 8. \' ==> Displays '
+* */
+
+import java.lang.*;
+
+class Program2Formatting{
+    public static void main( String args[] ){
+        int a = 1, b = 2, c = 3, d = 4;
+        System.out.print(a+"\t"+b);
+        System.out.println(b+"\n"+b);
+        System.out.print(":"+c);
+        System.out.println();
+        System.out.println("Hello\\HI\""+d);
+    }
+}
