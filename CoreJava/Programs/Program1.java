7d95d2b14b672d7dd670a31fb7a4eed5d567f69e
shubhamr10 <shubhamrauniyar10@gmail.com>
2021-07-05 08:43:31 +0530

    Program 1 : Find the sum

--- CoreJava/Programs/Program1.java
+++ CoreJava/Programs/Program1.java
@@ -1,1 +1,21 @@
-/* Write a Java Program to find
+/* Write a Java Program to find the Sum of of numbers */
+
+import java.lang.*;
+
+class Program1Sum{
+    public static void main( String args[]){
+//        Variable declaration
+        int x, y;
+//        Store declared values
+        x = 10;
+        y = 25;
+//        Calculate the sum and store it in the variable
+        int z = x+y; // This is also called dynamci declaration
+//        Unformatted Results
+        System.out.print(z);
+//        Formatted well
+        System.out.println();
+        System.out.println("Sum of numbers is:"+z);
+//        Trying to calculate and print on the same line
+    }
+}
